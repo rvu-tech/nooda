173fd7a8f9bc12c53b261c86dc4aba86f19b8527
Tom Booth <tom.booth@rvu.co.uk>
2023-07-19 11:46:24 +0100

    Add more complicated formatters for charts

---
+++ nooda/chart/formatter.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+from dateutil import relativedelta
+from matplotlib.ticker import FuncFormatter
+
+
+def _hours_to_day_hours(hours, *args):
+    delta = timedelta(hours=hours)
+    hours = round(delta.seconds / (60 * 60))
+
+    return f"{delta.days}d {hours}h"
+
+
+hours_to_day_hours = FuncFormatter(_hours_to_day_hours)
+
+
+def _seconds_to_day_hours(seconds, *args):
+    delta = timedelta(seconds=seconds)
+    hours = round(delta.seconds / (60 * 60))
+
+    return f"{delta.days}d {hours}h"
+
+
+seconds_to_day_hours = FuncFormatter(_seconds_to_day_hours)
+
+
+def human_readable_delta(delta: relativedelta) -> str:
+    attrs = ["years", "months", "days", "hours", "minutes", "seconds"]
+    human_readable = [
+        "%d %s"
+        % (getattr(delta, attr), attr if getattr(delta, attr) > 1 else attr[:-1])
+        for attr in attrs
+        if getattr(delta, attr)
+    ]
+
+    return ", ".join(human_readable)
