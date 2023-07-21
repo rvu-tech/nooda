783555ba60548963c43a4c00f03eb69530ece8cb
Tom Booth <tom.booth@rvu.co.uk>
2023-07-21 15:17:41 +0100

    Overhaul databricks metadata functions

--- nooda/vendor/databricks.py
+++ nooda/vendor/databricks.py
@@ -1,5 +1,8 @@
 import json
 
+from os.path import basename
+from typing import Optional
+
 
 def running_in_databricks() -> bool:
     try:
@@ -13,31 +16,74 @@ def running_in_databricks() -> bool:
         return False
 
 
-def notebook_url(dbutils=None) -> str:
-    if dbutils is None:
+def notebook_name(spark=None) -> str:
+    if spark is None:
         from pyspark.dbutils import DBUtils
         from pyspark.sql import SparkSession
 
         spark = SparkSession.builder.getOrCreate()
-        dbutils = DBUtils(spark)
 
+    dbutils = DBUtils(spark)
+
+    return basename(
+        dbutils.notebook.entry_point.getDbutils()
+        .notebook()
+        .getContext()
+        .notebookPath()
+        .get()
+    )
+
+
+def notebook_url(spark=None) -> str:
+    if spark is None:
+        from pyspark.dbutils import DBUtils
+        from pyspark.sql import SparkSession
+
+        spark = SparkSession.builder.getOrCreate()
+
+    dbutils = DBUtils(spark)
+
+    workspace_url = spark.conf.get("spark.databricks.workspaceUrl")
     notebook_config = json.loads(
         dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson()
     )["tags"]
 
-    return f"https://{notebook_config['browserHostName']}/?o={notebook_config['orgId']}#{notebook_config['browserHash']}"
+    return f"https://{workspace_url}/?o={notebook_config['orgId']}#notebook/{notebook_config['notebookId']}"
 
 
-def notebook_path(dbutils=None) -> str:
-    if dbutils is None:
+def job_url(spark=None) -> Optional[str]:
+    if spark is None:
         from pyspark.dbutils import DBUtils
         from pyspark.sql import SparkSession
 
         spark = SparkSession.builder.getOrCreate()
-        dbutils = DBUtils(spark)
 
-    context = json.loads(
+    dbutils = DBUtils(spark)
+
+    workspace_url = spark.conf.get("spark.databricks.workspaceUrl")
+    notebook_config = json.loads(
         dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson()
-    )
+    )["tags"]
 
-    return context["extraContext"]["notebook_path"]
+    if "jobId" in notebook_config and "multitaskParentRunId" in notebook_config:
+        return f"https://{workspace_url}/?o={notebook_config['orgId']}#job/{notebook_config['jobId']}/run/{notebook_config['multitaskParentRunId']}"
+    else:
+        return None
+
+
+def notebook_path(spark=None) -> str:
+    if spark is None:
+        from pyspark.dbutils import DBUtils
+        from pyspark.sql import SparkSession
+
+        spark = SparkSession.builder.getOrCreate()
+
+    dbutils = DBUtils(spark)
+
+    return (
+        dbutils.notebook.entry_point.getDbutils()
+        .notebook()
+        .getContext()
+        .notebookPath()
+        .get()
+    )
