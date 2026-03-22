0cbafe5a4bb49d0cc205af79e946bf68488fd3ed
Tom Booth <tom.booth@rvu.co.uk>
2026-03-22 20:44:20 +0000

    allow user to set Chart arguments from nooda.plot

--- nooda/__init__.py
+++ nooda/__init__.py
@@ -1,9 +1,24 @@
-from .chart.ops import Chart
+from typing import Callable, Optional, TypeVar
+
+import numpy as np
+from matplotlib.ticker import Formatter, StrMethodFormatter
+
+from .chart.ops import Chart, Plot
 
 
 def FinancialFormatter(symbol="$", dp=0):
     return symbol + "{x:,." + str(dp) + "f}"
 
 
-def plot(df):
-    return Chart().plot(df)
+T = TypeVar("T")
+
+
+def plot(
+    df,
+    *,
+    title: Optional[str] = None,
+    formatter: Formatter | str = StrMethodFormatter("{x:,.0f}"),
+    y_limits: Optional[tuple[float, float]] = None,
+    agg: Callable[[list[T]], T] = np.sum,
+):
+    return Chart(title=title, formatter=formatter, y_limits=y_limits, agg=agg).plot(df)
